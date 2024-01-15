5bc6172d4ff81ca6fe145a3a76178ec146d9c5e0
Makarios-Tech101 <ogodogublessing1@gmail.com>
2024-01-15 17:18:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const message = [
+  " Learn React✨",
+  " Apply for Jobs👜",
+  " Invest in yourself👸",
+
+];
+
+
+export default function App() {
+
+  const step = 1;
+
+  function handlePrevious() {
+
+  }
+
+  function handleNext() {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="step">
+       <div className="numbers">
+         <div className={`${step >= 1 ? "active": ""}`}>1</div>
+         <div className={`${step >= 2 ? "active": ""}`}>2</div>
+         <div className={`${step >= 3 ? "active": ""}`}>3</div>
+       </div>
+       <div className="sentence">
+         <p>
+           Step {step}: {message[step-1]}
+         </p>
+       </div>
+       <div className='buttons'>
+         <button 
+           style={{backgroundColor: "#50038e", color:"#fff" }}
+           onClick={handlePrevious}
         >
-          Learn React
-        </a>
-      </header>
+           Previous
+        </button>
+         <button 
+            style={{backgroundColor:"#50038e", color: "#fff" }}
+            onClick={handleNext}
+        >
+           Next
+        </button>
+       </div>
     </div>
   );
 }
-
-export default App;
